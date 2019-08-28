e1b4de4718912ae1110b543b536fd1bf2785db1d
Jason Lenoble <jason.lenoble@gmail.com>
2019-08-28 15:55:23 +0200

    Support string input

--- src/author.ts
+++ src/author.ts
@@ -4,10 +4,24 @@ export default interface Author {
   url?: string;
 }
 
+export type AuthorInput = string | Author;
+
+export const authorRegExp = /^\s*([^(<]+)(?:\s+<(.+)>)?(?:\s+\((.+)\))?\s*$/;
+
 export default class Author {
-  public constructor(author: Author) {
-    this.name = author.name;
-    this.email = author.email;
-    this.url = author.url;
+  public constructor(author: AuthorInput) {
+    if (typeof author === "string") {
+      const match = author.match(authorRegExp);
+
+      if (match) {
+        this.name = match[1];
+        this.email = match[2];
+        this.url = match[3];
+      }
+    } else {
+      this.name = author.name;
+      this.email = author.email;
+      this.url = author.url;
+    }
   }
 }
